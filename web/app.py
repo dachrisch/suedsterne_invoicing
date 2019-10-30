28ea3d0c4162a10af9351b7d82879eac4d906f1e
Christian Dähn <christian.daehn@gmail.com>
2019-10-30 20:20:29 +0100

    made web version

---
+++ web/app.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from dateutil.parser import parse
+from flask import Flask, render_template
+from flask import request
+
+from google_calendar.service import GoogleCalendarService
+from invoicing.billing import MonthlyBillingGenerator
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        date = parse("%s-%s-01" % (request.form.get('year'), request.form['month']))
+
+        billing = MonthlyBillingGenerator.with_service(GoogleCalendarService()).generate_billing(date.year,date.month)
+
+        return render_template('invoicing.html', billing=billing)
+
+    return '''<form method="POST">
+                  Year: <input type="text" name="year"><br>
+                  Month: <input type="text" name="month"><br>
+                  <input type="submit" value="Generate"><br>
+              </form>'''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
