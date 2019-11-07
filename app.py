eb9a6d5061a0febd4b96582e597e90d2bbef6306
Christian Dähn <christian.daehn@gmail.com>
2019-11-07 15:10:28 +0100

    datepicker for month selection

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 import os
 
-from dateutil.parser import parse
 from flask import Flask, render_template
-from flask import request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired
 
 from google_calendar.service import GoogleCalendarService
 from invoicing.billing import MonthlyBillingGenerator
 
-app = Flask(__name__)
+invoice_app = Flask(__name__)
+invoice_app.config['SECRET_KEY'] = 'secret'
+Bootstrap(invoice_app)
 
 
-@app.route("/", methods=['GET', 'POST'])
+class BillingMonthForm(FlaskForm):
+    date = DateField(u'Billing Month', validators=[DataRequired('please state month')])
+    submit = SubmitField('Submit')
+
+
+@invoice_app.route("/", methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        date = parse("%s-%s-01" % (request.form.get('year'), request.form['month']))
-
+    form = BillingMonthForm()
+    if form.validate_on_submit():
+        date = form.date.data
         billing = MonthlyBillingGenerator.with_service(GoogleCalendarService()).generate_billing(date.year, date.month)
 
         return render_template('invoicing.html', billing=billing)
 
-    return '''<form method="POST">
-                  Year: <input type="text" name="year"><br>
-                  Month: <input type="text" name="month"><br>
-                  <input type="submit" value="Generate"><br>
-              </form>'''
+    return render_template('month_chooser.html', form=form)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('INVOICE_SERVICE_HOST', '0.0.0.0'),
-            port=os.getenv('INVOICE_SERVICE_PORT', 5000))
+    invoice_app.run(host=os.getenv('INVOICE_SERVICE_HOST', '0.0.0.0'),
+                    port=os.getenv('INVOICE_SERVICE_PORT', 5000))
