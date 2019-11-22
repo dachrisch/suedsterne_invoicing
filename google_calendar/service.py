d1025d1859ada11c76f7f0c70a7239009ec55d49
Christian Dähn <christian.daehn@gmail.com>
2019-11-22 17:03:02 +0100

    removed service account --> now using google package

--- google_calendar/service.py
+++ google_calendar/service.py
@@ -1,10 +1,8 @@
 import calendar
-import pickle
 from datetime import datetime
 from os import path
 
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -13,7 +11,6 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 class GoogleCalendarService(object):
 
     def customer_events(self, year: int, month: int, template: str = 'Kunde: '):
-
         reference = datetime(year=year, month=month, day=1)
         start_of_month = reference.isoformat() + 'Z'
         end_of_month = (reference.replace(
@@ -24,27 +21,10 @@ class GoogleCalendarService(object):
                                                          orderBy='startTime', q=template).execute().get('items', [])
         return events
 
-    def __calendar_service(self):
-        """Shows basic usage of the Google Calendar API.
-            Prints the start and name of the next 10 events on the user's calendar.
-            """
-        credentials = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                credentials = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not credentials or not credentials.valid:
-            if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    path.join(path.join(path.expanduser('~'), '.credentials'), 'cal.json'), SCOPES)
-                credentials = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(credentials, token)
+    def __calendar_service(self, user_email='cd@it-agile.de'):
+        credentials = service_account.Credentials.from_service_account_file(
+            path.join(path.join(path.expanduser('~'), '.credentials'), 'suedsterne-1328.json'),
+            scopes=SCOPES).with_subject(user_email)
+
         service = build('calendar', 'v3', credentials=credentials)
         return service
